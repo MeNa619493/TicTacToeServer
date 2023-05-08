02768aeff39f369f54e6bb63fb2d3e7f24f97059
mohamed taha93 <mohamedtahadawoud@gmail.com>
2023-05-08 23:54:00 +0300

    Indentation

--- src/utilities/OnlinePlayer.java
+++ src/utilities/OnlinePlayer.java
@@ -25,109 +25,105 @@ import java.util.logging.Logger;
  *
  * @author Mina
  */
-public class OnlinePlayer extends Thread{
-   private Boolean loggedin;
-   private Server server;
-   private DataInputStream dis;
-   private PrintStream ps;
-   private Socket currentSocket;
+public class OnlinePlayer extends Thread {
 
-   private String clientData,query;
-   private StringTokenizer token;
-   Thread thread;
-   DataAccessLayer database;
-   ArrayList<OnlinePlayer> OnlineUsers = new ArrayList();
-   private String username;
-   private String password;
-   private String email ;
-   
+    private Boolean loggedin;
+    private Server server;
+    private DataInputStream dis;
+    private PrintStream ps;
+    private Socket currentSocket;
 
-    
-    public OnlinePlayer(Socket socket){
-       loggedin = false;
-       database = DataAccessLayer.getInstance();
-       System.out.println("start OnlinePlayer");
-       server = Server.getServer();
-       try {
+    private String clientData, query;
+    private StringTokenizer token;
+    Thread thread;
+    DataAccessLayer database;
+    ArrayList<OnlinePlayer> OnlineUsers = new ArrayList();
+    private String username;
+    private String password;
+    private String email;
+
+    public OnlinePlayer(Socket socket) {
+        loggedin = false;
+        database = DataAccessLayer.getInstance();
+        System.out.println("start OnlinePlayer");
+        server = Server.getServer();
+        try {
             dis = new DataInputStream(socket.getInputStream());
             ps = new PrintStream(socket.getOutputStream());
             currentSocket = socket;
             this.start();
-       }catch (IOException ex) {
-           System.out.println("problem in streams OnlinePlayer");
+        } catch (IOException ex) {
+            System.out.println("problem in streams OnlinePlayer");
             ex.printStackTrace();
             // alert 
-           try {
-               socket.close();
-           } catch (IOException e) {
-               System.out.println("problem in close socket in case IOException OnlinePlayer");
-               e.printStackTrace();
-           }
-       }
-   }
-    
-    private void sendRequest (){
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("problem in close socket in case IOException OnlinePlayer");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendRequest() {
         String secondPlayer = token.nextToken();
         String player1 = token.nextToken();
-        for (OnlinePlayer user :OnlineUsers ){
-            if(user.email.equals(secondPlayer)){
+        for (OnlinePlayer user : OnlineUsers) {
+            if (user.email.equals(secondPlayer)) {
                 user.ps.println("requestPlaying");
                 user.ps.println(secondPlayer);
-            
+
             }
-        
+
         }
-    
-    
+
     }
-    
-    private void SignUp(){
+
+    private void SignUp() {
         String username = token.nextToken();
-                            String email = token.nextToken();
-                            String password = token.nextToken();
-                            System.out.println(username+" "+email+" "+password);
-                             String check;
-       try {
-           check = database.validateRegister(email);
-           if(check.equals("Registered Successfully")){
-               // ps.println("Registered Successfully");
+        String email = token.nextToken();
+        String password = token.nextToken();
+        System.out.println(username + " " + email + " " + password);
+        String check;
+        try {
+            check = database.validateRegister(email);
+            if (check.equals("Registered Successfully")) {
+                ps.println("success");
 
-               database.signUp(email,username,password);
-               
-               System.out.println("User is registered now , check database");   
+                database.signUp(email, username, password);
 
-           }
-           else if (check.equals("already signed-up")){
-                //ps.println("already signed-up");
+                System.out.println("User is registered now , check database");
+
+            } else if (check.equals("already signed-up")) {
+                ps.println("already signed-up");
             }
-       } catch (Exception ex) {
-           Logger.getLogger(OnlinePlayer.class.getName()).log(Level.SEVERE, null, ex);
-       }
-                            
-    
+        } catch (Exception ex) {
+            Logger.getLogger(OnlinePlayer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-     public void run(){
-        if (server!=null){
-            while(currentSocket.isConnected()){
+
+    public void run() {
+        if (server != null) {
+            while (currentSocket.isConnected()) {
                 try {
                     clientData = dis.readLine();
-                    if(clientData != null){
+                    if (clientData != null) {
                         System.out.println(clientData);
-                        token = new StringTokenizer(clientData,"####");
+                        token = new StringTokenizer(clientData, "####");
                         query = token.nextToken();
-                        switch(query){
+                        switch (query) {
 
                             case "playerlist":
                                 pushAvliableFriend();
                                 break;
-                            case "SignUp" :
+                            case "SignUp":
                                 SignUp();
-                                    
 
-                            default :
+                            default:
                                 break;
                         }
-                   }
+                    }
                 } catch (IOException ex) {
                     System.out.println("Problem in reading from stream in OnlinePlayer");
                     ex.printStackTrace();
@@ -135,34 +131,34 @@ public class OnlinePlayer extends Thread{
                         currentSocket.close();
                         System.out.println("socket closed");
                     } catch (IOException e) {
-                         System.out.println("Problem in socket closed in OnlinePlayer");
-                         e.printStackTrace();
+                        System.out.println("Problem in socket closed in OnlinePlayer");
+                        e.printStackTrace();
                     }
                     this.stop();
                 }
             }
         }
     }
-     
-     public void pushAvliableFriend() {
-        thread =  new Thread(new Runnable() {
-        @Override
+
+    public void pushAvliableFriend() {
+        thread = new Thread(new Runnable() {
+            @Override
             public void run() {
-                while(true){
-                    if(ServerUiClass.serverState){
+                while (true) {
+                    if (ServerUiClass.serverState) {
                         List<String> availableFriends = server.database.showAvailableFriend();
 
-                            for(String name: availableFriends){
-                                ps.println(name+"###");
-                            }
-                            ps.println("null");
+                        for (String name : availableFriends) {
+                            ps.println(name + "###");
+                        }
+                        ps.println("null");
 
-                       try {
-                        Thread.sleep(5000);
+                        try {
+                            Thread.sleep(5000);
                         } catch (InterruptedException ex) {
                             System.out.println("Error while thread sleep pushAvliableFriend");
                         }
-                    }else{
+                    } else {
                         ps.println("close");
                         thread.stop();
                     }
@@ -171,7 +167,5 @@ public class OnlinePlayer extends Thread{
         });
         thread.start();
     }
-     
-     
-     
+
 }
